50029831a4c4518651c64c5f14d8df667702262b
Sarah Lindmar <srhcrete@gmail.com>
2017-08-22 10:49:30 -0700

    initialize index, styles, scripts, and  create form for users to select movie options

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//business logic
+function Ticket(movie, age, time) {
+
+}
+
+
+
+
+//user interface logic
+$(document).ready(function() {
+  $('form#checkMoviePrice').submit(function(event) {
+    event.preventDefault();
+  });
+});
